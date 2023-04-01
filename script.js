92e4c478b1c237950c0833e1e039a8cce4cf6bdb
Gabriel Branco <gabriel.live.2012@hotmail.com>
2023-04-01 00:49:04 -0300

    add sonic loss

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const sonic = document.querySelector(".sonic");
+const eggman = document.querySelector(".eggman");
 
 const jump = () => {
     sonic.classList.add("jump");
@@ -9,5 +10,20 @@ const jump = () => {
         sonic.src = "./arquivos/Sonic.gif"
     }, 900);
 }
+
+const loop = setInterval(() => {
+   const eggmanPosition = eggman.offsetLeft;
+   const sonicPosition = +window
+   .getComputedStyle(sonic)
+   .bottom.replace("px", "");
+
+   if (eggmanPosition < 110 && eggmanPosition > 0 && sonicPosition < 200) {
+    eggman.style.animation = "none";
+    eggman.style.left = `${eggmanPosition}px`;
+
+    sonic.style.animation = "none";
+    sonic.src = "./arquivos/Sonic-Loss.gif";
+   }
+},10)
 
 document.addEventListener("click", jump);
